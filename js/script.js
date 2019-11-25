600850a13ae002a4978fe2d92d6bcfbfd20731fb
Klaywert <klaywert.souza@ccc.ufcg.edu.br>
2019-11-25 18:12:19 -0300

    Adicionado mais métodos js, pequena pendencia em relacao a curtidas e comentários

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+async function verificaTokenExpirado(responseMessage) {
+  const responseTexto = await responseMessage.text()
+  const responseJson = await JSON.parse(responseTexto)
+
+  if (responseJson["message"].startsWith("Token expirado")) {
+    localStorage.removeItem("userToken")
+    localStorage.removeItem("userEmail")
+    alert("O seu token atual foi expirado, faça o login novamente para prosseguir")
+  }
+}
+
+const modSignIn = document.getElementById("signInMod");
+
+const discModal = document.getElementById("campanhaMod");
+
+
+function killAllChildren(elemento) {
+  let $discContainer = document.querySelector(elemento);
+  if (!$discContainer) return;
+  while ($discContainer.firstChild) {
+    $discContainer.removeChild($discContainer.firstChild);
+  }
+}
+
+const checaUsuarioLogado = function () {
+  if (localStorage.getItem("userToken") == null) {
+    alert("Faça login para prosseguir")
+    modSignIn.style.display = "flex";
+    return true;
+  }
+}
